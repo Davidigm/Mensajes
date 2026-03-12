53c068920ef75d36a6b868f7bdca5818979d020e
Davidigm <a22300926@ceti.mx>
2026-03-11 23:34:09 -0600

    se supone que hay imagenes pero no las veo

--- mensaje/src/app/servicios/products.service.ts
+++ mensaje/src/app/servicios/products.service.ts
@@ -8,12 +8,39 @@ export class ProductsService {
 
   productos: Product[] = [
 
-    { id:1, name:"Mouse Gamer", price:450, description:"Mouse RGB" },
-    { id:2, name:"Teclado Mecánico", price:900, description:"Switch Blue" },
-    { id:3, name:"Monitor", price:3000, description:"Monitor 24 pulgadas" },
-    { id:4, name:"Laptop", price:15000, description:"Laptop i5 16GB RAM" }
+ { 
+   id:1,
+   name:"Mouse Gamer",
+   price:450,
+   description:"Mouse RGB",
+   image:"assets/productos/mouse.jpg"
+ },
 
-  ];
+ { 
+   id:2,
+   name:"Teclado Mecánico",
+   price:900,
+   description:"Switch Blue",
+   image:"assets/productos/teclado.jpg"
+ },
+
+ { 
+   id:3,
+   name:"Monitor",
+   price:3000,
+   description:"Monitor 24 pulgadas",
+   image:"assets/productos/monitor.jpg"
+ },
+
+ { 
+   id:4,
+   name:"Laptop",
+   price:15000,
+   description:"Laptop i5 16GB RAM",
+   image:"assets/productos/laptop.jpg"
+ }
+
+];
 
   getProducts(){
 
